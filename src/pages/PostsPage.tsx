fab82e762e10761ff57cb64911680c78edaefb4d
NikBel3476 <nikita.belkin3476@gmail.com>
2022-11-25 16:13:59 +0400

    feature: add posts api

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,11 +1,28 @@
 import React, {FC} from 'react';
+import {postApi, useGetAllPostsQuery} from "../store/reducers/postApi";
+import {Post} from "../types/Post";
 
 const PostsPage: FC = () => {
-    return (
-        <div>
-            Posts page
-        </div>
-    );
+  const { data: posts, error, isLoading } = postApi.useGetAllPostsQuery({ page: 1, limit: 20});
+
+  return (
+      <div>
+        {isLoading && <h1>Loading...</h1>}
+        {error && <h1>Error on posts loading</h1>}
+        {posts && posts.map((post: Post) =>
+          <div
+            key={post.id}
+            style={{
+              marginTop: '20px',
+              border: '1px solid black',
+              padding: '5px'
+            }}
+          >
+            {post.body}
+          </div>
+        )}
+      </div>
+  );
 };
 
 export default PostsPage;
---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { postApi } from "./reducers/postApi";
+import {setupListeners} from "@reduxjs/toolkit/query";
+
+const rootReducer = combineReducers({
+	[postApi.reducerPath]: postApi.reducer
+})
+
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(postApi.middleware)
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
